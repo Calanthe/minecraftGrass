9c8fbefb2fc3bff0e86c07133344a26567b7cf50
Zofia Korcz <korczzofia@gmail.com>
2012-03-29 20:20:16 +0200

    first commit

---
+++ notransforms.js
@@ -0,0 +1,6 @@
+var replacement= 	"<h1>Your browser doesn't support CSS 3d</h1>"+
+						"<p>Please download newest "+
+						"<a href='http://www.google.com/chrome' 'title='download Chrome'>Chrome</a> or "+
+						"<a href='http://www.mozilla.org/en-US/firefox/fx/' 'title='download Firefox'>Firefox</a>.</p>";
+
+document.getElementById("stage").innerHTML = replacement;
